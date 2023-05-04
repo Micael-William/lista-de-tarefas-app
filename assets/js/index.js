d2fd04b38a6f00855a659fcbedd95fb850669e37
Micael-William <mw931991@gmail.com>
2023-05-04 14:39:04 -0300

    ajuste

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,73 @@
-import Conteudo from "../module/Conteudo.js";
-class Tarefa extends Conteudo  {
-    constructor(elemento,ul){
-        super(elemento,ul)
+class Tarefa  {
+    elemento = [];
+    ul;
+
+    constructor(){
+        this.ul = document.querySelector(".container");
+        this.elemento = this.elemento;
+    }
+
+    pegarValor(){
+        const input = document.querySelector(".campo-adicionar");
+        const valor = input.value;
+
+        
+        if(valor){
+            this.adicionar(valor);
+            this.criarView();
+            this.limparCampo(valor, input);
+        }else {
+            this.validaCampo(valor);
+        }
+    }
+
+    adicionar(input){
+        this.elemento.push({itemTarefa: input});
+    }
+
+    criarView(){
+        this.ul.innerHTML = ""
+        this.elemento.forEach((i, index) => {
+            this.ul.innerHTML += `
+                <li>
+                    <div class="lista">
+                        <img class="logo-concluir" src="/assets/img/verificar.png" onClick="concluir()" alt="logo-concluir">
+                        <span>${i.itemTarefa}</span>
+                    </div>
+                    <img class="logo-excluir" src="/assets/img/excluir (1).png" onClick="excluir(${index})" alt="logo-excluir">
+                </li>
+            `
+        })
+    }
+
+    validaCampo(valor){
+       if(valor == ""){
+            alert("O campo deve ser preenchido!");
+       }
+    }
+
+    // excluir(posicao){
+    //     conteudo.elemento.splice(posicao, 1);
+    // }
+
+    limparCampo(valor,input){
+        if(valor){
+            input.value = ""
+        }
     }
 }
 
 const tarefa = new Tarefa();
 
 
+const form = document.querySelector(".form");
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    tarefa.criarView();
+    tarefa.pegarValor();
+})
+
+function excluir(posicao){
+    const verifica = tarefa.elemento.splice(posicao, 1);
+    tarefa.criarView();
+}
