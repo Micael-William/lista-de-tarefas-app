f7b0858ad7cbb4b0a0b77d52b37fccb67ef63456
Micael-William <mw931991@gmail.com>
2023-05-04 14:18:38 -0300

    ajuste no projeto

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,10 @@
-class Tarefa {
-    Valor
-    elemento = []
-
-
-    constructor(Valor,elemento){
-        this.Valor = Valor
-        this.elemento = elemento
-        
+import Conteudo from "../module/Conteudo.js";
+class Tarefa extends Conteudo  {
+    constructor(elemento,ul){
+        super(elemento,ul)
     }
-
-   
-
-    pegarValor(){
-       return this.elemento.push(this.Valor)
-    }
-
-    
 }
 
-const botao = document.querySelector('.botao-adicionar')
-
-botao.addEventListener('click', chamar)
+const tarefa = new Tarefa();
 
 
-function chamar(){
-    const input = document.getElementById('adicionar-tarefa').value
-    const item = {
-        i: input
-    }
-    const valor = new Tarefa(input, item.i);
-    console.log(valor)
-    const dados = valor.pegarValor()
-    console.log(dados)
-}
